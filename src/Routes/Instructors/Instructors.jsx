efcdd1fce9dd85bb81fd60036ca9c3c0c5b33959
itsrazib1 <itsrazib1@gmail.com>
2023-06-11 09:16:07 +0600

    modify instrator page

--- src/Routes/Instructors/Instructors.jsx
+++ src/Routes/Instructors/Instructors.jsx
@@ -13,34 +13,31 @@ const Instructors = () => {
 
   return (
     <div >
-      
-      <div className="grid grid-cols-1 md:grid-cols-3 my-5 justify-items-center gap-4 ">
-        {instructors.map((instructor) => (
-          <>
-            <div className="card w-96 bg-[#bad3d0] shadow-xl">
-              <div className="card-body">
-              <figure>
-                <img src={instructor.image} alt={instructor.name} />
-              </figure>
-                <h2 className="card-title">{instructor.name}</h2>
-                <p>Email: {instructor.email}</p>
-                {instructor.classesTaken && (
-                <p >Number of Classes taken: {instructor.numClassesTaken}</p>
-              )}
-              {instructor.classNames && (
-                <p>
-                  Classes taken by the Instructor:{" "}
-                  {instructor.classesTaken.join(", ")}
-                </p>
-              )}
-              </div>
-
-              
-              
-            </div>
-          </>
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5 justify-items-center gap-4">
+  {instructors.map((instructor) => (
+    <>
+      <div className="card w-96 bg-[#f5e9e2] shadow-lg rounded-tr  shadow-red-500 rounded-bl">
+        <div className="card-body">
+          <figure>
+            <img src={instructor.image} alt={instructor.name} />
+          </figure>
+          <h2 className="card-title text-xl font-bold mt-4 mb-2">{instructor.name}</h2>
+          <p className="text-gray-600">{instructor.email}</p>
+          {instructor.classesTaken && (
+            <p className="mt-2">Number of Classes taken: {instructor.numClassesTaken}</p>
+          )}
+          {instructor.classNames && (
+            <p className="mt-2">
+              Classes taken by the Instructor: {instructor.classesTaken.join(", ")}
+            </p>
+          )}
+        </div>
       </div>
+    </>
+  ))}
+</div>
+
+      
     </div>
   );
 };
